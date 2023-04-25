697bff5c31bace2192bfea5ec28095fd753a1183
hazel1814 <terilolots@gmail.com>
2023-04-25 13:37:38 +0530

    Loading the application

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+
 
   async function fetchMovieHandler() {
+    setIsLoading(true)
     const response = await fetch('https://swapi.dev/api/films')
     const data = await response.json()
     const transformedMovies = data.results.map(movieData => {
@@ -17,7 +20,11 @@ function App() {
         releaseDate: movieData.release_date
       }
     })
+
     setMovies(transformedMovies)
+    setIsLoading(false)
+
+
 
   }
 
@@ -28,7 +35,9 @@ function App() {
         <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Found no movies</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
